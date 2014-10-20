53b3221f79c7f06613ba19b712dcfe84010f9c3b
Santiago <sdelatorrepi@umass.edu>
2014-10-20 19:01:14 -0400

    Right shifting in moodle.c

--- C/Class/moodle.c
+++ C/Class/moodle.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 //function prototypes
-int left_bitshifting(int number, int shifts);
-
+int left_bitshifting(int number, int numberOfShifts);
+int right_bitshifting(int number, int numberOfShifts);
 
 int main(){
 
-
-
-
-printf("%d\n", left_bitshifting(2,7) );
+// printf("%d\n", left_bitshifting(2,7) );//Left shifting
+printf("%d\n", right_bitshifting(4,1) );//right shifting
 
 }
 
 //Function Definitions
-int left_bitshifting(int number, int shifts){
+int left_bitshifting(int number, int numberOfShifts){
 	int x ;
-		x = number << shifts; 
+		x = number << numberOfShifts; 
+	return x;
+}
+int right_bitshifting(int number, int numberOfShifts){
+	int x;
+	x= number >> numberOfShifts;
 	return x;
 }
