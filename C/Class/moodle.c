4cda5eb2b048df17724ca16b7b7554bd7a0f8dfc
Santiago <sdelatorrepi@umass.edu>
2014-10-20 19:58:52 -0400

    update in twos_complement

--- C/Class/moodle.c
+++ C/Class/moodle.c
@@ -9,7 +9,7 @@ int main(){
 
 // printf("%d\n", left_bitshifting(2,7) );//Left shifting
 // printf("%d\n", right_bitshifting(4,1) );//right shifting
-   printf("%d\n", twos_complement(-247) );
+   printf("%d\n", twos_complement(17) );
 }
 
 //Function Definitions
@@ -20,5 +20,5 @@ int right_bitshifting(int number, int numberOfShifts){
 	return number >> numberOfShifts;
 }
 int twos_complement(int number){
-	return ~number | 1;
+	return ~number + 1;
 }
