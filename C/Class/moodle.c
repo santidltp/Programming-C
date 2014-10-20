19f9267840a7dcba5a531ef0c2c532c75ea8f5d3
Santiago <sdelatorrepi@umass.edu>
2014-10-20 19:41:38 -0400

    Twos complement function in moodle.c

--- C/Class/moodle.c
+++ C/Class/moodle.c
@@ -3,22 +3,22 @@
 //function prototypes
 int left_bitshifting(int number, int numberOfShifts);
 int right_bitshifting(int number, int numberOfShifts);
+int twos_complement(int number);
 
 int main(){
 
 // printf("%d\n", left_bitshifting(2,7) );//Left shifting
-printf("%d\n", right_bitshifting(4,1) );//right shifting
-
+// printf("%d\n", right_bitshifting(4,1) );//right shifting
+   printf("%d\n", twos_complement(-247) );
 }
 
 //Function Definitions
 int left_bitshifting(int number, int numberOfShifts){
-	int x ;
-		x = number << numberOfShifts; 
-	return x;
+	return  number << numberOfShifts; 
 }
 int right_bitshifting(int number, int numberOfShifts){
-	int x;
-	x= number >> numberOfShifts;
-	return x;
+	return number >> numberOfShifts;
+}
+int twos_complement(int number){
+	return ~number | 1;
 }
