05543ea963c6622e0dd874a8394eb3d245299374
Santiago <sdelatorrepi@umass.edu>
2014-10-20 21:33:19 -0400

    prinBinary

--- C/Class/moodle.c
+++ C/Class/moodle.c
@@ -4,12 +4,14 @@
 int left_bitshifting(int number, int numberOfShifts);
 int right_bitshifting(int number, int numberOfShifts);
 int twos_complement(int number);
+void printBinary(int number);
 
 int main(){
 
 // printf("%d\n", left_bitshifting(2,7) );//Left shifting
 // printf("%d\n", right_bitshifting(4,1) );//right shifting
-   printf("%d\n", twos_complement(17) );
+// printf("%d\n", twos_complement(17) );
+// printBinary(3);
 }
 
 //Function Definitions
@@ -21,4 +23,14 @@ int right_bitshifting(int number, int numberOfShifts){
 }
 int twos_complement(int number){
 	return ~number + 1;
+}
+void printBinary(int number){
+int i=0;
+	for(i=sizeof(int)-1; i>=0; --i){
+	if(number & (1<< i))
+		printf("1");
+	else
+		printf("0");
+	}
+	printf("\n");
 }
